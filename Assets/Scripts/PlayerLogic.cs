7a00df6d36aaf2348d715969e77c6c4c29440281
yadoter73 <tomanovpl@gmail.com>
2025-11-14 22:53:37 +0400

    1
    1

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -1,26 +1,30 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using System.Collections;
 
 public class PlayerLogic : MonoBehaviour
 {
     public float PlayerSpeed = 2.85f;
     public float MaxPlayerSpeed = 5.5f;
 
+    Ray rayToPlane;
+    int chek = 0;
+    Vector3 targetPoint;
+    float distance;
     void Update()
     {
         Vector3 mousePos = Input.mousePosition;
         if (Input.GetMouseButton(1))
         {
-            Ray rayToPlane = Camera.main.ScreenPointToRay(mousePos);
+            rayToPlane = Camera.main.ScreenPointToRay(mousePos);
             Plane plane = new Plane(Vector3.up, new Vector3(0, transform.position.y, 0));
-            float distance;
             if (plane.Raycast(rayToPlane, out distance))
             {
-                Vector3 targetPoint = rayToPlane.GetPoint(distance);
-                transform.position = Vector3.MoveTowards(transform.position, targetPoint, PlayerSpeed * Time.deltaTime);
+                targetPoint = rayToPlane.GetPoint(distance);
+                chek = 1;
             }
         }
-
+        if (chek == 1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPoint, PlayerSpeed * Time.deltaTime);
+        }
     }
 }
