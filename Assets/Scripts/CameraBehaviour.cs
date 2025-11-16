3f6f08b42fd8c402f95de9032fe5993724362cb0
yadoter73 <tomanovpl@gmail.com>
2025-11-17 01:49:13 +0400

    1
    1

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,26 +4,32 @@ using UnityEngine;
 public class CameraBehaviour : MonoBehaviour
 {
     [SerializeField] private float _screenSpeed = 5f;
+    [SerializeField] private GameObject alax;
     void Update()
     {
         Vector3 mousePos = Input.mousePosition;
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            transform.position = new Vector3(alax.transform.position.x, transform.position.y, alax.transform.position.z + 2);
+        }
 
         if (mousePos.x < 50f)
         {
             transform.Translate(Vector3.left * _screenSpeed * Time.deltaTime);
         }
-		else if (mousePos.x > Screen.width - 50f)
-		{
-			transform.Translate(Vector3.right * _screenSpeed * Time.deltaTime);
-		}
-		if (mousePos.y > Screen.height - 50f)
-		{
-			transform.Translate(Vector3.forward * _screenSpeed * Time.deltaTime);
-		}
-		else if (mousePos.y < 50f)
-		{
-			transform.Translate(Vector3.back * _screenSpeed * Time.deltaTime);
-		}
-		
-	}
+        else if (mousePos.x > Screen.width - 50f)
+        {
+            transform.Translate(Vector3.right * _screenSpeed * Time.deltaTime);
+        }
+        if (mousePos.y > Screen.height - 50f)
+        {
+            transform.Translate(Vector3.forward * _screenSpeed * Time.deltaTime);
+        }
+        else if (mousePos.y < 50f)
+        {
+            transform.Translate(Vector3.back * _screenSpeed * Time.deltaTime);
+        }
+
+    }
 }
