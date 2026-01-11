3a17cf6555bacca0b3e78cd73bc044556bc126b9
yadoter73 <tomanovpl@gmail.com>
2026-01-11 17:48:52 +0400

    Create 3DH FTT Path_001 Sand_001 1K.png.meta

---
+++ Assets/KinematicCharacterController/ExampleCharacter/Scripts/ExamplePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KinematicCharacterController;
+using KinematicCharacterController.Examples;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+namespace KinematicCharacterController.Examples
+{
+    public class ExamplePlayer : MonoBehaviour
+    {
+        private PlayerInput player_Input;
+        public UnityEvent OnPlayerMoveEvent { get; set; }
+        private void Awake()
+        {
+            player_Input = GetComponent<PlayerInput>();
+        }
+        public void OnPlayerMovement()
+        {
+
+        }
+    }
+}
