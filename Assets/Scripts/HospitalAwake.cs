54641b514fd04f85db8e414407b6920c55268ce4
yadoter73 <tomanovpl@gmail.com>
2026-01-17 17:24:30 +0300

    1
    1

--- Assets/Scripts/HospitalAwake.cs
+++ Assets/Scripts/HospitalAwake.cs
@@ -1,26 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 using PrimeTween;
-using System.Collections;
+using DialogueEditor;
 public class HospitalAwake : MonoBehaviour
 {
     private Image _image;
 	[SerializeField] Image _imageB;
+	[SerializeField] private NPCConversation _playerConversation;
 	private float duration = 1f;
 	private void Start()
 	{
 		_image = GetComponent<Image>();
-		Tween.Delay(1).OnComplete(() =>Changing());
-	}
-	IEnumerator Changing()
-	{
-		float time = 0f;
-		while (time < duration)
-		{
-			time += Time.deltaTime;
-			Color color = Color.Lerp(_image.color, _imageB.color, duration / time);
-			_image.color = color;
-			yield return null;
-		}
+		Tween.Delay(1).OnComplete(() => { Tween.Color(_image, _imageB.color, duration: duration, ease: Ease.OutQuad);
+									      ConversationManager.Instance.StartConversation(_playerConversation);
+		});
 	}
 }
