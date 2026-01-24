70dac2d7987513168fffa51ce0351025a6949f7b
yadoter73 <tomanovpl@gmail.com>
2026-01-24 21:31:34 +0400

    1
    1

--- Assets/Scripts/HospitalAwake.cs
+++ Assets/Scripts/HospitalAwake.cs
@@ -9,10 +9,13 @@ public class HospitalAwake : MonoBehaviour
 	[SerializeField] private NPCConversation _playerConversation;
 	private float duration = 1f;
 	private void Start()
-	{
+	{	
 		_image = GetComponent<Image>();
-		Tween.Delay(1).OnComplete(() => { Tween.Color(_image, _imageB.color, duration: duration, ease: Ease.OutQuad);
-									      ConversationManager.Instance.StartConversation(_playerConversation);
-		});
+		Color color = _image.color;
+		Color color2 = _imageB.color;
+		Sequence.Create()
+			.Chain(Tween.Color(_image, _imageB.color, duration: duration, ease: Ease.OutQuad))
+			.Chain(Tween.Color(_imageB, _image.color, duration: duration, ease: Ease.OutQuad));
+		ConversationManager.Instance.StartConversation(_playerConversation);
 	}
 }
