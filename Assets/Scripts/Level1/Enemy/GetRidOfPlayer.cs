db8cea4bff5b3a6e3dae3c0f21124c46c484702a
yadoter73 <tomanovpl@gmail.com>
2026-03-14 16:48:25 +0300

    1
    1

--- Assets/Scripts/Level1/Enemy/GetRidOfPlayer.cs
+++ Assets/Scripts/Level1/Enemy/GetRidOfPlayer.cs
@@ -2,17 +2,28 @@ using UnityEngine;
 using PrimeTween;
 public class GetRidOfPlayer : EnemyState
 {
-    public GetRidOfPlayer(EnemyAI enemy, ArenaBounds arenaBounds, LayerMask whatIsGround) : base(enemy, arenaBounds, whatIsGround) { }
-    public override void UpdateState()
-    {
-        Vector3 point = _enemy.FindPoint();
-        _enemy.MoveTo(point);
-		float distance = Vector3.Distance(_enemy.transform.position, point);
-
-		if (distance < 2f) 
-        {
-            Tween.Delay(3).OnComplete(() => new PatrolState(_enemy, _arenaBounds, _whatIsGround)); 
-        }
+	private bool _isWaiting = false;
+	private Vector3 point;
+	public GetRidOfPlayer(EnemyAI enemy, ArenaBounds arenaBounds, LayerMask whatIsGround) : base(enemy, arenaBounds, whatIsGround) 
+	{
+		point = _enemy.FindPoint();
+		_enemy.playerWaitingToLeave = false;
 	}
-    
+	public override void UpdateState()
+	{
+		_enemy.MoveTo(point);
+
+		float distance = Vector3.Distance(_enemy.transform.position, point);
+		if (distance < 2f && !_isWaiting)
+		{
+			_isWaiting = true;
+			Tween.Delay(4).OnComplete(() =>
+			{ 
+				_enemy.SwitchState(new PatrolState(_enemy, _arenaBounds, _whatIsGround));
+				_enemy.playerWaitingToLeave = false;
+			});
+
+		}
+	}
+
 }
--- Assets/Scripts/Level1/Enemy/LookingState.cs
+++ Assets/Scripts/Level1/Enemy/LookingState.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class LookingState : EnemyState
 {
 	private float enemyRotation = 5f;
+	private bool _isTransitioning = false;
+	private Tween _delayTween;
 	public LookingState(EnemyAI enemy, ArenaBounds arenaBounds, LayerMask whatIsGround) : base(enemy, arenaBounds, whatIsGround) { }
 	public override void UpdateState()
 	{
-		if (!_enemy.playerInAttackRange )
+		if (!_enemy.playerInAttackRange)
 		{
+			_delayTween.Stop();
 			_enemy.SwitchState(new ChaseState(_enemy, _arenaBounds, _whatIsGround));
 			return;
 		}
+
 		_enemy.MoveTo(_enemy.transform.position);
 		Vector3 direction = (_enemy.player.position - _enemy.transform.position).normalized;
 		Quaternion targetRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
 		_enemy.transform.rotation = Quaternion.Slerp(_enemy.transform.rotation, targetRotation, enemyRotation * Time.deltaTime);
-		Tween.Delay(3).OnComplete(() =>
+
+		if (!_isTransitioning)
 		{
-			_enemy.SwitchState(new GetRidOfPlayer(_enemy, _arenaBounds, _whatIsGround));
-			_enemy.playerWaitingToLeave = true;
-		});
+			_isTransitioning = true;
+			_delayTween = Tween.Delay(3).OnComplete(() =>
+			{
+				_enemy.playerWaitingToLeave = true;
+				_enemy.SwitchState(new GetRidOfPlayer(_enemy, _arenaBounds, _whatIsGround));
+			});
+		}
+
 	}
 
 }
