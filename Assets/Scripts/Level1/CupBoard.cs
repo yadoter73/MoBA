9c62f6a10feb3c1cf8894fd20e149911debc926a
yadoter73 <tomanovpl@gmail.com>
2026-04-04 17:29:04 +0300

    1
    1

--- Assets/Scripts/Level1/CupBoard.cs
+++ Assets/Scripts/Level1/CupBoard.cs
@@ -1,16 +1,26 @@
+using PrimeTween;
 using UnityEngine;
 
 public class CupBoard : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private bool _isOpen = false;
+	[SerializeField] private Vector3 _vector3End;
+	private Vector3 _startVector3;
+	private void Start()
+	{
+		_startVector3 = transform.position;
+	}
+	public void Interact(int id)
+	{
+		if (!_isOpen)
+		{
+			Tween.Position(this.transform, endValue: _vector3End, duration: 0.5f, ease: Ease.InOutBack)
+				.OnComplete(() => _isOpen = true);
+		}
+		else
+		{
+			Tween.Position(this.transform, endValue: _startVector3, duration: 1f, ease: Ease.InOutBack)
+				.OnComplete(() => _isOpen = false);
+		}
+	}
 }
